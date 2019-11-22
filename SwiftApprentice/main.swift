2bad3906c261c0671ca95f960d152ac0ce6edf97
Sunaina Vallamkonda <svallamkonda@lcs.on.ca>
2019-11-22 14:33:28 -0500

    Initial Commit

---
+++ SwiftApprentice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftApprentice
+//
+//  Created by Vallamkonda, Sunaina on 2019-11-22.
+//  Copyright © 2019 Vallamkonda, Sunaina. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
